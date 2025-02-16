23edab4dc3b79ac585624441ce9208ad2bf024e3
Kshitij Satija <kshitijsatija003@gmail.com>
2025-02-17 01:02:19 +0530

    updated user model

--- models/user.model.js
+++ models/user.model.js
@@ -3,8 +3,13 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
+  email: {type: String, required: true, unique: true},
   password: { type: String, required: true },
-  role: { type: String, default: "junior" }, // Default role
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  mobileno: { type: Number, required: true, unique: true},
+  dob: { type: Date, required: true },
+  role: { type: String, default: "user" }, 
 });
 
 
